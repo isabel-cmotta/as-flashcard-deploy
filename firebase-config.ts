2386c74ffe67e76a8e7e57eba11ad9578f04c24a
Isabel Motta <icavalcantemotta@gmail.com>
2024-05-26 20:49:49 -0300

    add to repository

---
+++ firebase-config.ts
@@ -0,0 +1,12 @@
+// firebaseConfig.ts
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIRESTORE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIRESTORE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIRESTORE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIRESTORE_APP_ID
+  };
+  
+  export default firebaseConfig;
---
+++ src/server.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from 'express';
+import admin from 'firebase-admin';
+import path from 'path';
+import dotenv from 'dotenv';
+
+// Carrega variáveis de ambiente do arquivo .env
+dotenv.config();
+
+// Validação das variáveis de ambiente
+const requiredEnvVariables = [
+  'FIREBASE_TYPE',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_PRIVATE_KEY_ID',
+  'FIREBASE_PRIVATE_KEY',
+  'FIREBASE_CLIENT_EMAIL',
+  'FIREBASE_CLIENT_ID',
+  'FIREBASE_AUTH_URI',
+  'FIREBASE_TOKEN_URI',
+  'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
+  'FIREBASE_CLIENT_X509_CERT_URL',
+  'FIREBASE_DATABASE_URL'
+];
+
+requiredEnvVariables.forEach(variable => {
+  if (!process.env[variable]) {
+    throw new Error(`Missing required environment variable: ${variable}`);
+  }
+});
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE as string,
+  project_id: process.env.FIREBASE_PROJECT_ID as string,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID as string,
+  private_key: (process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')) as string,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL as string,
+  client_id: process.env.FIREBASE_CLIENT_ID as string,
+  auth_uri: process.env.FIREBASE_AUTH_URI as string,
+  token_uri: process.env.FIREBASE_TOKEN_URI as string,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL as string,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL as string,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+const db = admin.firestore();
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+// Servindo arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const snapshot = await db.collection('flashcard-admin').get();
+    const data = snapshot.docs.map(doc => doc.data());
+    res.json(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send('Error getting documents: ' + error.message);
+    } else {
+      res.status(500).send('Unknown error occurred');
+    }
+  }
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor Node.js rodando na porta ${PORT}`);
+});
