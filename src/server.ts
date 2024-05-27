8cade0f40ac736f72fae2637a7c5a692dd56bf65
Isabel Motta <icavalcantemotta@gmail.com>
2024-05-26 22:16:04 -0300

    server executed

--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,6 @@ const requiredEnvVariables = [
   'FIREBASE_PRIVATE_KEY',
   'FIREBASE_CLIENT_EMAIL',
   'FIREBASE_CLIENT_ID',
-  'FIREBASE_AUTH_URI',
-  'FIREBASE_TOKEN_URI',
-  'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
-  'FIREBASE_CLIENT_X509_CERT_URL',
   'FIREBASE_DATABASE_URL'
 ];
 
@@ -31,13 +27,9 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE as string,
   project_id: process.env.FIREBASE_PROJECT_ID as string,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID as string,
-  private_key: (process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')) as string,
+  private_key: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL as string,
   client_id: process.env.FIREBASE_CLIENT_ID as string,
-  auth_uri: process.env.FIREBASE_AUTH_URI as string,
-  token_uri: process.env.FIREBASE_TOKEN_URI as string,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL as string,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL as string,
 };
 
 admin.initializeApp({
@@ -70,3 +62,6 @@ app.get('/', async (req: Request, res: Response) => {
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor Node.js rodando na porta ${PORT}`);
 });
+
+
+//ts-node src/server.ts
